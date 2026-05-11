48628a762dc9144b2b227ba42366c15245960918
Varshan G <varshan2908@gmail.com>
2026-05-12 00:38:04 +0530

    Update database password in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Varsh@2466",
+  password: "PASSWORD",
   database: "smart_campus"
 });
 
